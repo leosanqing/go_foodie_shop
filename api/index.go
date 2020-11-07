8d92104c213ccb7612445348d91eb7ac952d44de
leosanqing <stormleo@qq.com>
2020-11-07 12:18:57 +0800

    1. update 更改结构

--- api/index.go
+++ api/index.go
@@ -3,33 +3,42 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"go-foodie-shop/service"
-	"strconv"
 )
 
 // Carousel 轮播图展示列表
 func Carousel(c *gin.Context) {
-	c.JSON(200, service.QueryCarouselList())
+	var indexService = service.IndexService{}
+	if err := c.ShouldBind(&indexService); err == nil {
+		c.JSON(200, indexService.QueryCarouselList())
+	} else {
+		c.JSON(200, ErrorResponse(err))
+	}
 }
 
 // Cats 查询以及目录下的所有分类
 func Cats(c *gin.Context) {
-	c.JSON(200, service.QueryAllRootLevelCats())
+	var indexService = service.IndexService{}
+	if err := c.ShouldBind(&indexService); err == nil {
+		c.JSON(200, indexService.QueryAllRootLevelCats())
+	} else {
+		c.JSON(200, ErrorResponse(err))
+	}
 }
 
 // SubCats 查询子分类(二三级)
 func SubCats(c *gin.Context) {
-	rootCatIdStr := c.Param("rootCatId")
-	if id, err := strconv.Atoi(rootCatIdStr); err == nil {
-		c.JSON(200, service.QuerySubCats(id))
+	var indexService = service.IndexService{}
+	if err := c.ShouldBind(&indexService); err == nil {
+		c.JSON(200, indexService.QuerySubCats(c.Param("rootCatId")))
 	} else {
 		c.JSON(200, ErrorResponse(err))
 	}
 }
 
 func GetSixNewItems(c *gin.Context) {
-	rootCatIdStr := c.Param("rootCatId")
-	if id, err := strconv.Atoi(rootCatIdStr); err == nil {
-		c.JSON(200, service.QuerySixNewItems(id))
+	var indexService = service.IndexService{}
+	if err := c.ShouldBind(&indexService); err == nil {
+		c.JSON(200, indexService.QuerySixNewItems(c.Param("rootCatId")))
 	} else {
 		c.JSON(200, ErrorResponse(err))
 	}
