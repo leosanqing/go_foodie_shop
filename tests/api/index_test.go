38c0687e1855bf29d139fb394349440d6f59f61c
leosanqing <stormleo@qq.com>
2020-11-07 11:49:12 +0800

    1. update 调整测试步骤

--- tests/api/index_test.go
+++ tests/api/index_test.go
@@ -21,12 +21,18 @@ func TestQueryCarousel(t *testing.T) {
 	assert.Equal(t, 200, w.Code)
 
 	var res serializer.Response
-
 	_ = json.Unmarshal([]byte(w.Body.String()), &res)
-	//var carousels []model.Carousel
-	carousels := res.Data.([]interface{})
-	assert.Equal(t, 4, len(carousels))
+	var carousels []model.Carousel
+	_ = gconv.SliceStruct(res.Data, &carousels)
 
+	assert.Equal(t, 4, len(carousels))
+	assert.Equal(t, "c-10011", carousels[0].Id)
+	assert.Equal(t, "http://122.152.205.72:88/group1/M00/00/05/CpoxxF0ZmG-ALsPRAAEX2Gk9FUg848.png", carousels[0].ImageUrl)
+	assert.Equal(t, "nut-1004", carousels[0].ItemId)
+	for i, carousel := range carousels {
+		assert.Equal(t, 1, carousel.IsShow)
+		assert.Equal(t, 1+i, carousel.Sort)
+	}
 }
 
 func TestQueryCats(t *testing.T) {
@@ -39,12 +45,15 @@ func TestQueryCats(t *testing.T) {
 	assert.Equal(t, 200, w.Code)
 
 	var res serializer.Response
-
 	_ = json.Unmarshal([]byte(w.Body.String()), &res)
-	//var carousels []model.Carousel
-	cats := res.Data.([]interface{})
-	assert.Equal(t, 10, len(cats))
+	var cats []model.Category
+	_ = gconv.SliceStruct(res.Data, &cats)
 
+	assert.Equal(t, 10, len(cats))
+	for _, cat := range cats {
+		assert.Equal(t, 0, cat.FatherId)
+		assert.Equal(t, 1, cat.Type)
+	}
 }
 
 func TestQuerySubCats(t *testing.T) {
@@ -94,7 +103,6 @@ func TestSixNewItems(t *testing.T) {
 	assert.Equal(t, 200, w.Code)
 
 	var res serializer.Response
-
 	var sixItems []model.NewItemsVO
 	_ = json.Unmarshal([]byte(w.Body.String()), &res)
 	_ = gconv.SliceStruct(res.Data, &sixItems)
