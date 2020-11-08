66cee61fc88cdfc5dd27f1845387c3040453d75e
leosanqing <stormleo@qq.com>
2020-11-08 22:35:25 +0800

    1. 调整商品详情接口，添加日志

--- api/item.go
+++ api/item.go
@@ -8,50 +8,85 @@ import (
 	"go-foodie-shop/model"
 	"go-foodie-shop/serializer"
 	"go-foodie-shop/service"
+	"go.uber.org/zap"
 )
 
 // ItemInfo 商品详情
 func ItemInfo(c *gin.Context) {
 	var itemVO model.ItemInfoVO
 	var itemService service.QueryItemService
+	if err := c.ShouldBind(&itemService); err == nil {
+		itemId := c.Param("itemId")
+		err, items := itemService.QueryItemsById(itemId)
+		if err != nil {
+			fmt.Println(items)
+			log.ServiceLog.Error("query Item by Id err id ")
+			c.JSON(200, ErrorResponse(errors.New("查询商品失败")))
+			return
+		}
+		itemVO.Item = items
 
-	itemId := c.Param("itemId")
-	err, items := itemService.QueryItemsById(itemId)
-	if err != nil {
-		fmt.Println(items)
-		log.ServiceLog.Error("query Item by Id err id ")
-		c.JSON(200, ErrorResponse(errors.New("查询商品失败")))
-		return
+		err, itemImg := itemService.QueryItemsImgById(itemId)
+		if err != nil {
+			log.ServiceLog.Error("query ItemImg by Id err id ")
+			c.JSON(200, ErrorResponse(errors.New("查询商品失败")))
+			return
+		}
+		itemVO.ItemImgList = itemImg
+
+		err, specs := itemService.QueryItemSpec(itemId)
+		if err != nil {
+			log.ServiceLog.Error("query itemSpec by Id err id %s")
+			c.JSON(200, ErrorResponse(errors.New("查询商品规格信息失败")))
+			return
+		}
+		itemVO.ItemSpecList = specs
+
+		err, itemParam := itemService.QueryItemsParam(itemId)
+		if err != nil {
+			log.ServiceLog.Error(
+				"query itemParam by Id err id ",
+				zap.String("itemId", itemId),
+				zap.Error(err),
+			)
+			c.JSON(200, ErrorResponse(errors.New("查询商品参数信息失败")))
+			return
+		}
+		itemVO.ItemParam = itemParam
+		log.ServiceLog.Info("query itemParam by Id err id ", zap.Any("itemParam", itemVO.ItemParam))
+
+		c.JSON(200, serializer.Response{
+			Status: 200,
+			Data:   itemVO,
+			Msg:    "success",
+		})
+	} else {
+		c.JSON(200, ErrorResponse(err))
 	}
-	itemVO.Item = items
+}
 
-	err, itemImg := itemService.QueryItemsImgById(itemId)
-	if err != nil {
-		log.ServiceLog.Error("query ItemImg by Id err id %s")
-		c.JSON(200, ErrorResponse(errors.New("查询商品失败")))
-		return
+// CommentLevelCounts 评价统计
+func CommentLevelCounts(c *gin.Context) {
+	var commentService service.CommentService
+	if err := c.ShouldBind(&commentService); err == nil {
+		if commentLevelVO, err := commentService.GetCommentsCount(); err != nil {
+			log.ServiceLog.Error(
+				"查询商品评价失败",
+				zap.String("itemId", commentService.ItemId),
+				zap.Error(err),
+			)
+			c.JSON(200, ErrorResponse(errors.New("查询商品评价失败")))
+		} else {
+			log.ServiceLog.Info(
+				"查询商品评价成功",
+				zap.Any("commentLevelVO", commentLevelVO),
+			)
+			c.JSON(200, serializer.Response{
+				Status: Success,
+				Data:   commentLevelVO,
+			})
+		}
+	} else {
+		c.JSON(4001, ErrorResponse(err))
 	}
-	itemVO.ItemImgList = itemImg
-
-	err, specs := itemService.QueryItemSpec(itemId)
-	if err != nil {
-		log.ServiceLog.Error("query itemSpec by Id err id %s")
-		c.JSON(200, ErrorResponse(errors.New("查询商品规格信息失败")))
-		return
-	}
-	itemVO.ItemSpecList = specs
-
-	err, itemParam := itemService.QueryItemsParam(itemId)
-	if err != nil {
-		log.ServiceLog.Error("query itemParam by Id err id %s")
-		c.JSON(200, ErrorResponse(errors.New("查询商品参数信息失败")))
-		return
-	}
-	itemVO.ItemParam = itemParam
-
-	c.JSON(200, serializer.Response{
-		Status: 200,
-		Data:   itemVO,
-	})
-	//c.JSON(200, itemService.ItemInfo())
 }
--- model/item.go
+++ model/item.go
@@ -5,23 +5,18 @@ import "time"
 type ItemInfoVO struct {
 	Item         Items       `json:"item"`
 	ItemImgList  []ItemsImg  `json:"itemImgList"`
-	ItemParam    ItemsParam  `json:"itemParam"`
+	ItemParam    ItemsParam  `json:"itemParams"`
 	ItemSpecList []ItemsSpec `json:"itemSpecList"`
 }
 
 type Items struct {
-	Id       string `gorm:"primary_key;not null" json:"id"`
-	ItemName string `json:"itemName"`
-	// 分类外键
-	CatId int `json:"catId"`
-	// 一级分类外键
-	RootCatId int `json:"rootCatId"`
-	// 累计销量
-	SellCounts int `json:"sellCounts"`
-	// 上下架状态 1.上架 2.下架
-	OnOffStatus int `json:"onOffStatus"`
-	// 商品内容
-	Content string `json:"content"`
+	Id          string `gorm:"primary_key;not null" json:"id"`
+	ItemName    string `json:"itemName"`
+	CatId       int    `json:"catId"`       // 分类外键
+	RootCatId   int    `json:"rootCatId"`   // 一级分类外键
+	SellCounts  int    `json:"sellCounts"`  // 累计销量
+	OnOffStatus int    `json:"onOffStatus"` // 上下架状态 1.上架 2.下架
+	Content     string `json:"content"`     // 商品内容
 
 	CreatedTime time.Time `json:"createdTime"`
 	UpdatedTime time.Time `json:"updatedTime"`
