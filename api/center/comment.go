fda81f5f52afcaa8f6b6e69e48347a322d386c04
leosanqing <stormleo@qq.com>
2020-11-16 17:52:28 +0800

    1. add 查询我的评价接口 实现

--- api/center/comment.go
+++ api/center/comment.go
@@ -7,9 +7,11 @@ import (
 	"go-foodie-shop/middleware/log"
 	"go-foodie-shop/serializer"
 	"go-foodie-shop/service"
+	"go-foodie-shop/util"
 	"go.uber.org/zap"
 )
 
+// Pending 查询我的订单动态
 func Pending(c *gin.Context) {
 	var orderRequest = service.QueryOrderRequest{}
 	if err := c.ShouldBind(&orderRequest); err == nil {
@@ -28,6 +30,33 @@ func Pending(c *gin.Context) {
 		c.JSON(200, serializer.Response{
 			Status: 200,
 			Data:   orderItems,
+		})
+	} else {
+		c.JSON(400, api.ErrorResponse(err))
+	}
+}
+
+// QueryMyComment 查询我的评价
+func QueryMyComment(c *gin.Context) {
+	var queryMyCommentRequest = service.QueryMyCommentRequest{}
+	if err := c.ShouldBind(&queryMyCommentRequest); err == nil {
+		commentVOS, total, err := queryMyCommentRequest.QueryMyComment()
+		if err != nil {
+			log.ServiceLog.Error(
+				"查询订单失败",
+				zap.String("userId", queryMyCommentRequest.UserId),
+				zap.Error(err),
+			)
+			c.JSON(200, api.ErrorResponse(errors.New("查询订单失败")))
+			return
+		}
+
+		log.ServiceLog.Info("查询订单成功", zap.Any("commentVOS", commentVOS))
+		result := util.PagedGridResult(commentVOS, total, queryMyCommentRequest.Page.Page, queryMyCommentRequest.PageSize)
+
+		c.JSON(200, serializer.Response{
+			Status: 200,
+			Data:   result,
 		})
 	} else {
 		c.JSON(400, api.ErrorResponse(err))
