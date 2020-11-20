667ac8ce8ef2f9aa1b6bed2e992534d0778dd241
leosanqing <stormleo@qq.com>
2020-11-20 11:21:51 +0800

    1. update 修改类型

--- model/order_status.go
+++ model/order_status.go
@@ -2,7 +2,7 @@ package model
 
 type OrderStatus struct {
 	OrderId     string     `gorm:"primary_key;not null" json:"orderId"`
-	OrderStatus string     `json:"orderStatus"`
+	OrderStatus int        `json:"orderStatus"`
 	CreatedTime *LocalTime `json:"createdTime"`
 	PayTime     *LocalTime `json:"payTime"`
 	SuccessTime *LocalTime `json:"successTime"`
