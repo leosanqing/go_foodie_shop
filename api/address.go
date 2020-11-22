83082750ca662f1fa37c6f7ad899a2641200fef5
leosanqing <stormleo@qq.com>
2020-11-22 14:40:14 +0800

    1. add 添加收货地址 接口 及单测

--- api/address.go
+++ api/address.go
@@ -1,23 +1,35 @@
 package api
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"go-foodie-shop/serializer"
 	"go-foodie-shop/service"
 )
 
+// QueryAllAddress 查询全部地址信息
 func QueryAllAddress(c *gin.Context) {
 	var indexService = service.QueryAllAddressRequest{}
 	if err := c.ShouldBindQuery(&indexService); err == nil {
-		header := c.GetHeader("headerUserId")
-		fmt.Println(header)
 		address, err := indexService.QueryAllAddress()
 		if err != nil {
 			c.JSON(200, ErrorResponse(err))
 			return
 		}
 		c.JSON(200, serializer.Response{Status: Success, Data: address})
+	} else {
+		c.JSON(400, ErrorResponse(err))
+	}
+}
+
+func AddAddress(c *gin.Context) {
+	var addAddressRequest = service.AddAddressRequest{}
+	if err := c.ShouldBindJSON(&addAddressRequest); err == nil {
+		err := addAddressRequest.AddNewUserAddress()
+		if err != nil {
+			c.JSON(200, ErrorResponse(err))
+			return
+		}
+		c.JSON(200, serializer.Response{Status: Success})
 	} else {
 		c.JSON(400, ErrorResponse(err))
 	}
